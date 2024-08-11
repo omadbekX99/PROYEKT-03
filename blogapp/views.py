7e21e28777c3ff3a57684ed49af4f128d3f11e3d
omadbekX99 <omadbekxaydarov978@.com>
2024-08-11 20:51:48 +0500

    yangi fayl

--- blogapp/views.py
+++ blogapp/views.py
@@ -7,10 +7,10 @@ from django.views.generic import ListView
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 5
-    template_name = "blog/post/list.html"
+    paginate_by = 2
+    template_name = "blogapp/post/list.html"
 
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published", publish__yead=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blogapp/post/detail.html', {'post': post})
